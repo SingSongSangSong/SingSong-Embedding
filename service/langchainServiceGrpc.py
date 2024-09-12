bf3a1ed73b0d13376618e8b214134da045d23a99
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-09-12 14:32:26 +0900

    Feat: changed song_name to page_content

--- service/langchainServiceGrpc.py
+++ service/langchainServiceGrpc.py
@@ -217,7 +217,7 @@ class LangChainServiceGrpc(LangchainRecommendServicer):
                 # SimilarItem 메시지 생성 및 추가
                 similar_items.append(SimilarItem(
                     songInfoId=result.get("song_info_id"),  # song_info_id
-                    songName=result.get("song_name"),  # 노래 제목
+                    songName=result.page_content,  # 노래 제목
                     singerName=result.get("artist_name"),  # 아티스트 이름
                     isMr=result.get("MR", False),  # MR 여부
                     ssss=result.get("ssss", ""),  # 추가 메타데이터 필드
