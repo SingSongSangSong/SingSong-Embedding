5757a9af1cd4831aaa55630787233079d8e421bb
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-10-05 18:25:27 +0900

    Feat: added function calling

--- testingGrpc/client_functionCalling.py
+++ testingGrpc/client_functionCalling.py
@@ -4,32 +4,29 @@ import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 import grpc
-from proto.langchainAgentRecommend.langchainAgentRecommend_pb2_grpc import LangchainAgentRecommendStub
-from proto.langchainAgentRecommend.langchainAgentRecommend_pb2 import LangchainAgentRequest
+from proto.functionCallingRecommend.functionCallingRecommend_pb2 import FunctionCallingRequest, FunctionCallingResponse
+from proto.functionCallingRecommend.functionCallingRecommend_pb2_grpc import functionCallingRecommendStub
 
 def run():
     # gRPC 서버에 연결
     with grpc.insecure_channel('localhost:50051') as channel:
         # LangchainRecommendStub을 사용해 서버와 통신
-        stub = LangchainAgentRecommendStub(channel)
+        stub = functionCallingRecommendStub(channel)
 
         # LangchainRequest 생성
-        request = LangchainAgentRequest(
+        request = FunctionCallingRequest(
             memberId=8,  # 예시로 memberId 8을 사용
             command="윤하의 사건의지평선과 비슷한 노래 추천해줘"  # 서버에 전달할 명령
         )
 
         # 서버에 요청을 보내고 응답 받기
-        response = stub.GetLangchainAgentRecommendation(request)
+        response = stub.GetFunctionCallingRecommendation(request)
 
         # 응답 처리
         print("Received Langchain Recommendation:")
-        for item in response.searchResult:
-            print(f"Song Info ID: {str(item.songInfoId)}\n")
-            print(f"Song Name: {item.reason}\n")
-            print(f"Singer Name: {item.singerName}\n")
-            print("-" * 20)
-            print("\n")
+        for item in response.songInfoId:
+            print(f"Song Info ID: {str(item)}\n")
+            
 
 if __name__ == "__main__":
     run()
