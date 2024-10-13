b22b737431db73d6d40ff14c695120c48f7dae82
sunwupark <sunwu5678@gmail.com>
2024-10-13 11:00:20 +0900

    feat: add dd trace on python application

--- main.py
+++ main.py
@@ -15,14 +15,11 @@ from service.hotTrendingService import HotTrendingService
 from service.tjCrawlingService import TJCrawlingService
 from apscheduler.schedulers.background import BackgroundScheduler
 import threading
-from flask import Flask, jsonify  # Flask 임포트
 
 # 로깅 설정
 logging.basicConfig(level=logging.DEBUG)  # Set to DEBUG for more detailed output
 logger = logging.getLogger(__name__)
 
-# Flask 앱 생성
-app = Flask(__name__)
 grpc_server_running = False  # gRPC 서버 상태 추적 변수
 
 # gRPC 서버를 실행하는 함수
@@ -48,28 +45,13 @@ def serve_grpc():
         logger.info("Scheduler and gRPC server stopped.")
         grpc_server_running = False  # gRPC 서버가 중지되었음을 표시
 
-# Flask Health Check 엔드포인트
-@app.route("/health", methods=["GET"])
-def health_check():
-    if grpc_server_running:
-        return jsonify({"status": "gRPC server running"}), 200
-    else:
-        return jsonify({"status": "gRPC server not running"}), 503
-
-# Flask 서버를 실행하는 함수
-def run_flask():
-    app.run(host="0.0.0.0", port=8080)
-
 # Milvus insert 실행 함수
 def run_milvus_insert_script():
     logger.info("Running Milvus insert process...")
-    
     # MilvusInsertService 인스턴스 생성
     milvus_service = MilvusInsertService(collection_name="singsongsangsong_22286")
-    
     # 데이터 삽입 프로세스 실행
     milvus_service.run(song_info_path="dataframe/song_info.csv", data_path="dataframe/with_ssss_22286_updated5.csv")
-
     logger.info("Milvus insert process completed successfully.")
 
 # 매 45분마다 실행할 작업 (UserProfileService의 run 메서드 호출)
@@ -79,10 +61,6 @@ def job():
     logger.info("User profile creation process finished.")
 
 if __name__ == "__main__":
-    # Flask 서버를 백그라운드 스레드에서 실행
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.start()
-
     # Milvus insert 작업을 메인 스레드에서 실행
     logger.info("Running Milvus insert process...")
     run_milvus_insert_script()  # 스레드로 실행하지 않고, 순차적으로 실행
@@ -110,6 +88,3 @@ if __name__ == "__main__":
 
     # gRPC 서버 종료 대기
     grpc_thread.join()
-
-    # Flask 서버 종료 대기
-    flask_thread.join()
